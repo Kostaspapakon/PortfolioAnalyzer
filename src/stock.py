58a9415f667a498df96ba987fae3c70a3bfd8f95
Kostaspapakon <t8240275@aueb.gr>
2026-06-24 15:46:53 +0300

    Made a constructor for Stock class

--- src/stock.py
+++ src/stock.py
@@ -1,2 +1,17 @@
 import yfinance as yf
 import pandas as pd
+
+class Stock: 
+    def __init__(self, ticker: str):
+        self.ticker = ticker
+        self.prices = None
+        self.returns = None
+    
+    def load_data(self, start="2020-01-01", end=None):
+        # Download historical price data from Yahoo Finance
+        data = yf.download(self.ticker, start=start, end=end)
+
+        # Keep only closing prices
+        self.prices = data["Close"]
+
+        return self.prices
